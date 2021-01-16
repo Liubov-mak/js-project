ebc9ab1154e8e4f037f7df81acc26aeb6416a6f1
Любовь Макарова <arsenii07@gmail.com>
2021-01-16 13:57:37 +0300

    16/01/2021

--- src/modules/callbackPopUp.js
+++ src/modules/callbackPopUp.js
@@ -1,42 +1,30 @@
 const callbackPopUp = () => {
-	const callbackBtn = document.querySelectorAll('.callback-btn'),
-		modalCallback = document.querySelector('.modal-callback'),		
-        modalOverlay = document.querySelector('.modal-overlay'),
-		buttonServices = document.querySelector('.button-services'),
-		servicesElements = document.querySelector('.services-elements'),
-		element = servicesElements.querySelectorAll('.element'),		  
-        modalClose = document.querySelector('.modal-close'); 
+	const modalCallback = document.querySelector('.modal-callback'),		
+        modalOverlay = document.querySelector('.modal-overlay'),		
+        body = document.querySelector('body');        
 
-        const actionsPopUp = () => {
+        const showPopUp = () => {
             modalCallback.style.display = 'block';
             modalOverlay.style.display = 'block';
             modalCallback.classList.add('b-show');
             modalOverlay.classList.add('b-show');
         };
-	
-		callbackBtn.forEach(elem => elem.addEventListener('click', () => {			
-			actionsPopUp();
-        }));
-        buttonServices.addEventListener('click', () => {
-            event.preventDefault();
-            actionsPopUp();
-        });         
-        modalClose.addEventListener('click', () => {
+
+        const hidePopUp = () => {
             modalCallback.style.display = 'none';
 			modalOverlay.style.display = 'none';
-        });
-        modalOverlay.addEventListener('click', () => {
-            modalCallback.style.display = 'none';
-			modalOverlay.style.display = 'none';
-		});	
-		element.forEach(elem => elem.addEventListener('click', (event) => {
-			event.preventDefault();
-			let target = event.target;
-			if(target.closest('.element')) {
-				event.preventDefault();
-				actionsPopUp();				
-			}
-		}));
+        };
+
+        body.addEventListener('click', (event) => {
+            let target = event.target;
+            if(target.classList.contains('button-services') || target.closest('.element') || target.classList.contains('callback-btn')) {
+                event.preventDefault();
+                showPopUp();
+            }
+            if(target.closest('.modal-close') || target.classList.contains('modal-overlay')) {
+                hidePopUp();
+            }     
+        });		
 };
 
 export default callbackPopUp;
